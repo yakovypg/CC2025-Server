26cb805ec33ac85fcf827fffcc37f56d021cdcb6
yakovypg <yasha-pmp@mail.ru>
2025-07-26 21:45:08 +0300

    Update ject.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: "node",
   transform: {
     ...tsJestTransformCfg
