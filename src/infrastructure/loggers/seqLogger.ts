9f23c9e801a14fb0c76519d98bb15f4790a54756
yakovypg <yasha-pmp@mail.ru>
2025-07-26 18:08:20 +0300

    Fix setting the default value when there is no environment variable

--- src/infrastructure/loggers/seqLogger.ts
+++ src/infrastructure/loggers/seqLogger.ts
@@ -6,8 +6,8 @@ import { name, version } from "../../../package.json";
 const createSeqLogger = (): Logger => {
   const logLevel = process.env.LOG_LEVEL ?? "info";
   const serverUrl = process.env.SEQ_SERVER_URI ?? "http://localhost:5341";
-  const batchSizeLimit = Number(process.env.LOGGER_BATCH_SIZE_LIMIT) ?? 50;
-  const maxBatchingTimeMs = Number(process.env.LOGGER_MAX_BATCHING_TIME_MS) ?? 1000;
+  const batchSizeLimit = Number(process.env.LOGGER_BATCH_SIZE_LIMIT) || 50;
+  const maxBatchingTimeMs = Number(process.env.LOGGER_MAX_BATCHING_TIME_MS) || 1000;
 
   const seqStream = createStream({
     serverUrl,
